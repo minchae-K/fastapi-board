b61f4cd046f363b1b5fc33ba0ca5c503d607ac6b
greenpea88 <green_pea88@naver.com>
2021-11-26 18:22:57 +0900

    :building_construction: CHANGE: user관련 api들을 board에서 분리

--- board/rest/routers/board.py
+++ board/rest/routers/board.py
@@ -3,12 +3,11 @@ from fastapi import APIRouter
 from sqlalchemy.orm import sessionmaker
 
 from board.repositories import engine
-from board.repositories.base import Base
-from board.repositories.models import DBUser, DBPost
+from board.repositories.models import DBPost
 
 from datetime import datetime
 
-from board.rest.models.board import User, Post, ModifyUserInfo
+from board.rest.models.board import Post
 
 router = APIRouter()
 
@@ -31,49 +30,6 @@ class MakeSession:
 def l7ConnectionCheck():
     return "success"
 
-@router.post("/join_user")
-def joinUser(user: User):
-    # Base.metadata.create_all(engine)
-
-    with MakeSession() as session:
-        new_user = DBUser()
-        new_user.name = user.name
-        new_user.email = user.email
-        new_user.password = user.password
-
-        session.add(new_user)
-        session.commit()
-
-        result = session.query(DBUser).all()
-    return result
-
-@router.put("/modify_user")
-def modifyUser(user_id: int, info: ModifyUserInfo):
-    #user data 수정
-    with MakeSession() as session:
-        user = session.query(DBUser).filter_by(id=user_id).first()
-        #filter_by로 넣으면 like 연산으로 들어가는 것?
-
-        if info.name != None:
-            user.name = info.name
-        if info.password != None:
-            user.password = info.password
-
-        session.add(user)
-        session.commit()
-    return user
-
-@router.put("/inactivate_user")
-def inactivateUser(user_id: int):
-    #user를 비활성화
-    with MakeSession() as session:
-        user = session.query(DBUser).filter_by(id=user_id).first()
-
-        user.inactivate = True
-
-        session.add(user)
-        session.commit()
-    return user
 
 @router.post("/upload_post")
 def uploadPost(post: Post):
